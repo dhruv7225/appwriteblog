1c741e2178089e4c143b90564334407c5e16a600
dhruv7225 <dhruvp7225@gmail.com>
2025-05-13 16:33:52 +0530

    index import change

--- src/components/index.js
+++ src/components/index.js
@@ -21,7 +21,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "../container/Container.jsx";
 import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn";
+import LogoutBtn from "../components/Header/LogoutBtn";
 import RTE from "./RTE.jsx";
 import Signup from "./Signup";
 import Login from "./Login";
